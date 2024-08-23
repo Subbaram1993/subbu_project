dad35607df3a44f0cb1cc4e5c5c46ca31c48ffd0
Subbaramaiah Chevuru <ram.subaram@gmail.com>
2024-08-23 22:34:49 +0530

    Updated the build scripts and other
    Activity:subbu_project

---
+++ scripts/jenkins_git_tagger.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Jenkins URL and credentials
+JENKINS_URL="https://your-jenkins-instance.com"
+JENKINS_USERNAME="your-username"
+JENKINS_PASSWORD="your-password-or-api-token"
+
+# Jenkins job name
+JENKINS_JOB_NAME="your-job-name"
+
+# Git repository URL
+GIT_REPO_URL="https://your-git-repo.com/your-repo-name.git"
+
+# Git tag prefix
+GIT_TAG_PREFIX="v"
+
+# Trigger Jenkins build
+curl -X POST \
+  "$JENKINS_URL/job/$JENKINS_JOB_NAME/build" \
+  -u "$JENKINS_USERNAME:$JENKINS_PASSWORD" \
+  -H 'Content-Type: application/json' \
+  -d '{}'
+
+# Get the latest build number from Jenkins
+BUILD_NUMBER=$(curl -X GET \
+  "$JENKINS_URL/job/$JENKINS_JOB_NAME/lastBuild/buildNumber" \
+  -u "$JENKINS_USERNAME:$JENKINS_PASSWORD" \
+  -H 'Content-Type: application/json')
+
+# Calculate the version numbers
+MAJOR_VERSION=$(($BUILD_NUMBER / 10000))
+MINOR_VERSION=$(($BUILD_NUMBER % 10000 / 100))
+PATCH_VERSION=$(($BUILD_NUMBER % 100))
+
+# Pad minor and patch versions with leading zeros if less than 10
+if [ $MINOR_VERSION -lt 10 ]; then
+  MINOR_VERSION="0$MINOR_VERSION"
+fi
+if [ $PATCH_VERSION -lt 10 ]; then
+  PATCH_VERSION="0$PATCH_VERSION"
+fi
+
+# Create a new tag in the Git repository
+TAG_NAME="$GIT_TAG_PREFIX$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION"
+git tag -a "$TAG_NAME" -m "Release $TAG_NAME"
+
+# Push the new tag to the Git repository
+git push --tags "$GIT_REPO_URL"
