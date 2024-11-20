2be1ffb43dd5b4114b45b72e49e0f40a92e92f45
Subbaram1993 <ram.subaram@gmail.com>
2024-11-20 06:39:53 +0530

    Add artifacts for version v0.3.5

---
+++ artifacts/archive/build/CMakeFiles/subbu_project_sum_of_unique_array_elements.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/subbu_project_sum_of_unique_array_elements.dir/src/sum_of_unique_array_elements.c.o"
+  "CMakeFiles/subbu_project_sum_of_unique_array_elements.dir/src/sum_of_unique_array_elements.c.o.d"
+  "subbu_project_sum_of_unique_array_elements"
+  "subbu_project_sum_of_unique_array_elements.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/subbu_project_sum_of_unique_array_elements.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
