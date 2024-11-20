4741610f81bce9d15e68b4bc9a3361a457bfea8d
Subbaram1993 <ram.subaram@gmail.com>
2024-11-20 19:39:18 +0530

    Add artifacts and Incremented the TAG version v0.4.5

---
+++ artifacts/archive/artifacts/archive/artifacts/archive/build/CMakeFiles/subbu_project_bitwise_add_subtraction_operators.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for subbu_project_bitwise_add_subtraction_operators.
+# This may be replaced when dependencies are built.
---
+++ artifacts/archive/artifacts/archive/artifacts/archive/build/CMakeFiles/subbu_project_maximum_element.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/subbu_project_maximum_element.dir/src/maximum_element_in_Array.c.o"
+  "CMakeFiles/subbu_project_maximum_element.dir/src/maximum_element_in_Array.c.o.d"
+  "subbu_project_maximum_element"
+  "subbu_project_maximum_element.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/subbu_project_maximum_element.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
