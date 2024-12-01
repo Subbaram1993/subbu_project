9b9b881f6b701638de9d2c126bd6b51b4619044d
Subbaram1993 <ram.subaram@gmail.com>
2024-12-01 12:49:24 +0530

    Add artifacts and Incremented the TAG version v1.1.5

---
+++ artifacts/archive/artifacts/archive/OtherFiles/jenkins_git_tagger.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Jenkins URL and credentials
+JENKINS_URL="http://localhost:8080/job/Subbu_Project_Build/"
+JENKINS_USERNAME="subbaramaiah_chevuru"
+JENKINS_PASSWORD="Subbu@8374127261"
+
+# Jenkins job name
+JENKINS_JOB_NAME="Subbu_Project_Build"
+
+# Git repository URL
+GIT_REPO_URL="https://github.com/Subbaram1993/subbu_project.git"
+
+# Git tag prefix
+GIT_TAG_PREFIX="v"
+
+# Trigger Jenkins build
+curl -X POST \
+  "$JENKINS_URL/job/$JENKINS_JOB_NAME/build" \
+  -u "$JENKINS_USERNAME:$JENKINS_PASSWORD" \
+  -H 'Content-Type: application/json' \
+  -d '{}'
+
+# Get the latest build number from Jenkins
+BUILD_NUMBER=$(curl -X GET \
+  "$JENKINS_URL/job/$JENKINS_JOB_NAME/lastBuild/buildNumber" \
+  -u "$JENKINS_USERNAME:$JENKINS_PASSWORD" \
+  -H 'Content-Type: application/json')
+
+# Calculate the version numbers
+MAJOR_VERSION=$(($BUILD_NUMBER / 10000))
+MINOR_VERSION=$(($BUILD_NUMBER % 10000 / 100))
+PATCH_VERSION=$(($BUILD_NUMBER % 100))
+
+# Pad minor and patch versions with leading zeros if less than 10
+if [ $MINOR_VERSION -lt 10 ]; then
+  MINOR_VERSION="0$MINOR_VERSION"
+fi
+if [ $PATCH_VERSION -lt 10 ]; then
+  PATCH_VERSION="0$PATCH_VERSION"
+fi
+
+# Create a new tag in the Git repository
+TAG_NAME="$GIT_TAG_PREFIX$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION"
+git tag -a "$TAG_NAME" -m "Release $TAG_NAME"
+
+# Push the new tag to the Git repository
+git push --tags "$GIT_REPO_URL"
---
+++ artifacts/archive/artifacts/archive/README.md
@@ -0,0 +1,4 @@
+# subbu_project
+This is the new Repository for the C programs. Will Add more in coming days.
+
+Thank You for Visiting....!!!!
