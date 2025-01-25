37ed408fff8caa87aaff25269a8658ef0903d301
Subbaramaiah Chevuru <ram.subaram@gmail.com>
2025-01-25 11:19:23 +0530

    Added the vertical triangle pattern program.
    Activity:subbu_project

---
+++ src/pattern_gen.c
@@ -0,0 +1,46 @@
+/****************************************************************/
+/*	Name	:	Pattern Printing			*/
+/*	Author	:	Subbaramaiah Chevuru			*/
+/*								*/
+/*1								*/
+/*1 3								*/
+/*1 3 5								*/
+/*1 3 5 7							*/
+/*1 3 5 7 9							*/
+/*2 4 6 8 10							*/
+/*2 4 6 8							*/
+/*2 4 6								*/
+/*2 4								*/
+/*2								*/
+/****************************************************************/
+
+//Header Files
+#include <stdio.h>
+
+//Main starts Here
+int main()
+{
+	int i,j,k,n=10;
+
+	//Printing the Upper part of the pattern
+	for(i=1;i<=n/2;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("%d ", (2*j-1));
+		}
+		printf("\n");
+	}
+
+	//Printing the Lower part of the pattern
+	for(i=n/2;i>=1;i--)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("%d ", (2*j));
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
